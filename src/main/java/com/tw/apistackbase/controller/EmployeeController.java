b9214c934286c635c155dbb69be8250293138f87
Eddie Fong <fonged@oocl.com>
2018-11-27 17:29:40 +0800

    add employees Class and init function

--- src/main/java/com/tw/apistackbase/controller/EmployeeController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -1,12 +1,12 @@
 package com.tw.apistackbase.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Created by jxzhong on 18/08/2017.
@@ -16,46 +16,18 @@ import java.util.logging.Logger;
 public class EmployeeController {
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
-
+    List<Employee> employees = new ArrayList<>();
     @GetMapping(path = "", produces = {"application/json"})
-    public ResponseEntity<String> getAll() {
-        String result = " [\n" +
-                "    {\n" +
-                "      \"id\": 0,\n" +
-                "      \"name\": \"Xiaoming\",\n" +
-                "      \"age\": 20,\n" +
-                "      \"gender\": \"Male\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"id\": 1,\n" +
-                "      \"name\": \"Xiaohong\",\n" +
-                "      \"age\": 19,\n" +
-                "      \"gender\": \"Female\"  \n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"id\": 2,\n" +
-                "      \"name\": \"Xiaozhi\",\n" +
-                "      \"age\": 15,\n" +
-                "      \"gender\": \"Male\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"id\": 3,\n" +
-                "      \"name\": \"Xiaogang\",\n" +
-                "      \"age\": 16,\n" +
-                "      \"gender\": \"Male\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"id\": 4,\n" +
-                "      \"name\": \"Xiaoxia\",\n" +
-                "      \"age\": 15,\n" +
-                "      \"gender\": \"Female\"\n" +
-                "    }\n" +
-                "  ]";
-
-
-        return ResponseEntity.ok(result);
+    public ResponseEntity<List<Employee>> getAll() {
+        return ResponseEntity.ok(employees);
     }
 
+    @GetMapping(path = "/init", produces = {"application/json"})
+    public ResponseEntity<List<Employee>> initAll() {
+
+        this.employees = new ArrayList<Employee>(java.util.Arrays.asList(new Employee("A","A",1,'A'),new Employee("B","B",2,'B')));
+        return ResponseEntity.ok(employees);
+    }
 
 
 
